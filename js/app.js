d54e8ea9de1a5c09f868db8ef1dbcaebe14d8054
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-08-03 09:01:56 -0400

    plus sign functionality

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     //Takes the name & habit of user and adds it to the list
     $(".add").click(function() {
         var userInput = $(".input").val();
-        $(".items").append('<p><button type="submit" class="add"><i class="fa fa-plus" aria-hidden="true"></i></button>&nbsp;<button class="minus"><i class="fa fa-minus" aria-hidden="true"></i></button>' + userInput + '<button class="trash"><i class="fa fa-trash" aria-hidden="true"></i></button></p>');
+        $(".items").append('<p><button class="plus"><i class="fa fa-plus" aria-hidden="true"></i></button>&nbsp;<button class="minus"><i class="fa fa-minus" aria-hidden="true"></i></button>' + userInput + '<button class="trash"><i class="fa fa-trash" aria-hidden="true"></i></button></p>');
         $(".input").val('');
     });
 
@@ -13,8 +13,17 @@ $(document).ready(function() {
         $(this).parent().remove();
     });
 
-    //plus button functionality, will add to progress bar
+    //plus button, will add to progress bar
+    $('body').on('click', '.plus', function() {
+    	$('.progress-bar').animate({width:'5%'});
+    });
+
+    //minus button will subtract from progress bar
+    $('body').on('click', '.minus', function() {
+    	$('.progress-bar').animate({width:-'3%'});
+    });
 });
 
 //Progress bar, if it starts at 0 and the plus button being pressed builds up funds to spend on rewards, great way to reinforce good habits
 //However, if the minus button is pressed, can't very well go into negative funds...does it stay at 0 until the plus button is pressed again?
+//Perhaps an if/else statement...
